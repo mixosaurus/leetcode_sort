30d2c03a20dd6e2fa62b097f1638f6013de2b889
snake <lcpdfk@163.com>
2023-09-13 12:00:35 +0800

    颜色分类

--- Sort_Colors/solution.cpp
+++ Sort_Colors/solution.cpp
@@ -3,44 +3,21 @@
 #include<algorithm>
 
 void Solution::sortColors(vector<int>& nums) {
-	quickSortApart(nums,0,nums.size()-1);
+	quickSort(nums,0,nums.size()-1);
 }
 
-// 快速排序，率辉《数据结构》版本
-void Solution::quickSortTraditional(vector<int>&nums,int start,int end) {
+// 此部分代码在所有的快速排序皆相同，可搭配不同的划分算法
+void Solution::quickSort(vector<int>&nums,int start,int end) {
 	if (start >= end) {
 		return;
 	}
-	// 暂存枢轴的值，而非其索引
-	int pivot_value = nums[start];
-	int i = start;
-	int j = end;
-	while (i < j) {
-		// j向左移动，直到其指向一个小于枢轴的值（仅在i<j时移动）
-		while (i < j && nums [j] >= pivot_value){
-			j--;
-		}
-		if (i < j) {
-			// 将比枢轴大元素置于枢轴左侧的空位，此时nums[j]可被视作空位
-			nums[i] = nums[j];
-			// i向右移动
-			i++;
-		}
-		while (i < j && nums[i] < pivot_value) {
-			i++;
-		}
-		if (i < j) {
-			nums[j] = nums[i];
-			j--;
-		}
-	}
-	// 将枢轴的值放回序列，此时i和j相遇
-	nums[i] = pivot_value;
-	quickSortTraditional(nums,start,i-1);
-	quickSortTraditional(nums,i+1,end);
+	int pivot_index = partitionTraditional(nums, start, end);
+	quickSort(nums, start, pivot_index - 1);
+	quickSort(nums, pivot_index + 1, end);
 }
 
-int Solution::partition(vector<int>& nums, int srart, int end) {
+// 快速排序的划分操作，率辉《数据结构》版本
+int Solution::partitionTraditional(vector<int>& nums, int start, int end) {
 	// 暂存枢轴的值，而非其索引
 	int pivot_value = nums[start];
 	int i = start;
@@ -69,16 +46,7 @@ int Solution::partition(vector<int>& nums, int srart, int end) {
 	return i;
 }
 
-// 快速排序，划分与递归分离版本
-void Solution::quickSortApart(vector<int>& nums, int start, int end) {
-	if (start < end) {
-		int pivot_index = partitionWithTwoPointers(nums, start, end);
-		quickSortApart(nums, start, pivot_index - 1);
-		quickSortApart(nums, pivot_index + 1, end);
-	}
-}
-
-// 快慢指针
+// 快速排序的划分操作，使用快慢指针
 int Solution::partitionWithTwoPointers(vector<int>& nums, int srart, int end) {
 	int pivot = nums[end];
 	// follow跟随i从左向右移动
--- Sort_Colors/solution.h
+++ Sort_Colors/solution.h
@@ -7,7 +7,7 @@ class Solution {
 public:
 	void sortColors(vector<int>& nums);
 	void quickSortTraditional(vector<int>& nums, int i, int j);
-	void quickSortApart(std::vector<int>& nums, int start, int end);
+	int partitionTraditional(vector<int>& nums, int srart, int end);
 	int partitionWithTwoPointers(std::vector<int>& nums, int srart, int end);
 };
 
